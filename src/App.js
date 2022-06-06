33daff1ab1935026b8f01fbab2ef1a89364f1ca1
ahlem <ahlembrahmi120@gmail.com>
2022-06-06 18:41:31 +0200

    jsx

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-
 import './App.css';
-
-
+import video from './MyVideo.mp4'
+import image from './imageInSrc.jpg'
+import './style.css';
 function App() {
   return (
     <div className="container">
 
      <div style={{border:"solid 1px Red",maxWidth:"100vw"}}/>
 
- <h1 className="title white">Ahlem brahmi </h1>
-
- <br />
-  
-
- <img id="im" src="imageInPublic.jpg" alt="Im"/>
+ <h1 className="title red">Ahlem brahmi </h1>
 
  <br />
 
- <img id= "im1" src="imageInSrc.jpg" alt="Im1"/>
-
+ <img id="im" src="imageInPublic.jpg" alt="im"/>
+<img id ="image" src={image} alt="image" />
  <br />
- < iframe title="react video" src = "https://www.youtube.com/embed/uXWycyeTeCs" largeur = {1000} hauteur = {500} > </ iframe >    
-
-
+ <br />
+ {/* <iframe title="react video" src = "https://www.youtube.com/embed/uXWycyeTeCs" largeur = {1000} hauteur = {500} > </ iframe >     */}
+ <video width="320" height="240" controls>
+ <source src={video} type="video/mp4" />
+</video>
 
     </div>
   );
